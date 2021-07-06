744d62c5bc29b1e0245f1eac0462bd375a6d3b53
Randy Flores <flores.randy91@gmail.com>
2021-07-06 00:56:23 -0400

    Add the Toolbar and the Estimate Form View

---
+++ src/components/CustomerForm.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        THIS IS THE ESTIMATE FORM
+    </div>
+</template>
---
+++ src/views/EstimateForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <customer-form />
+</template>
+
+<script>
+  import CustomerForm from '../components/CustomerForm.vue'
+
+  export default {
+    name: 'Estimate Form',
+
+    components: {
+      CustomerForm,
+    },
+  }
+</script>
