b57d1d3900eb3c0787867bc324b8b4cc497f5920
Randy Flores <flores.randy91@gmail.com>
2021-07-10 23:18:35 -0400

    Add Getters and Setters for Personal Information

--- src/components/CustomerPersonalInformation.vue
+++ src/components/CustomerPersonalInformation.vue
@@ -1,5 +1,171 @@
 <template>
   <v-sheet rounded="lg">
-    PERSONAL BOX
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-form ref="form" class="mx-5">
+            <v-text-field
+              v-model="name"
+              :rules="nameRules"
+              label="Full Name"
+              required
+            ></v-text-field>
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-sheet>
 </template>
+
+<script>
+import { mapGetters, mapMutations } from "vuex";
+
+export default {
+  data: () => ({
+    nameRules: [
+      (name) => !!name || "Name is required",
+      (v) => (v && v.length >= 3) || "Name must be more than 3 characters",
+    ],
+  }),
+  components: {},
+  computed: {
+    ...mapGetters({
+      getCoApplicantName: "getCoApplicantName",
+      getCompanyName: "getCompanyName",
+      getDate: "getDate",
+      getHouseToPaintAddress: "getHouseToPaintAddress",
+      getHouseToPaintCity: "getHouseToPaintCity",
+      getHouseToPaintState: "getHouseToPaintState",
+      getHouseToPaintZipcode: "getHouseToPaintZipcode",
+      getIsHouseToPaintSameAsPersonal: "getIsHouseToPaintSameAsPersonal",
+      getName: "getName",
+      getPersonalAddress: "getPersonalAddress",
+      getPersonalCity: "getPersonalCity",
+      getPersonalState: "getPersonalState",
+      getPersonalZipcode: "getPersonalZipcode",
+    }),
+    coApplicantName: {
+      get() {
+        return this.getCoApplicantName;
+      },
+      set(val) {
+        this.$store.commit(this.setCoApplicantName(val));
+      },
+    },
+    companyName: {
+      get() {
+        return this.getCompanyName;
+      },
+      set(val) {
+        this.$store.commit(this.setCompanyName(val));
+      },
+    },
+    date: {
+      get() {
+        return this.getDate;
+      },
+      set(val) {
+        this.$store.commit(this.setDate(val));
+      },
+    },
+    houseToPaintAddress: {
+      get() {
+        return this.getHouseToPaintAddress;
+      },
+      set(val) {
+        this.$store.commit(this.setHouseToPaintAddress(val));
+      },
+    },
+    houseToPaintCity: {
+      get() {
+        return this.getHouseToPaintCity;
+      },
+      set(val) {
+        this.$store.commit(this.setHouseToPaintCity(val));
+      },
+    },
+    houseToPaintState: {
+      get() {
+        return this.getHouseToPaintState;
+      },
+      set(val) {
+        this.$store.commit(this.setHouseToPaintState(val));
+      },
+    },
+    houseToPaintZipcode: {
+      get() {
+        return this.getHouseToPaintZipcode;
+      },
+      set(val) {
+        this.$store.commit(this.setHouseToPaintZipcode(val));
+      },
+    },
+    isHouseToPaintSameAsPersonal: {
+      get() {
+        return this.getIsHouseToPaintSameAsPersonal;
+      },
+      set(val) {
+        this.$store.commit(this.setIsHouseToPaintSameAsPersonal(val));
+      },
+    },
+    name: {
+      get() {
+        return this.getName;
+      },
+      set(val) {
+        console.log(val);
+        this.setName(val);
+      },
+    },
+    personalAddress: {
+      get() {
+        return this.getPersonalAddress;
+      },
+      set(val) {
+        this.$store.commit(this.setPersonalAddress(val));
+      },
+    },
+    personalCity: {
+      get() {
+        return this.getPersonalCity;
+      },
+      set(val) {
+        this.$store.commit(this.setPersonalCity(val));
+      },
+    },
+    personalState: {
+      get() {
+        return this.getPersonalState;
+      },
+      set(val) {
+        this.$store.commit(this.setPersonalState(val));
+      },
+    },
+    personalZipcode: {
+      get() {
+        return this.getPersonalZipcode;
+      },
+      set(val) {
+        this.$store.commit(this.setPersonalZipcode(val));
+      },
+    },
+  },
+  methods: {
+    ...mapMutations({
+      setCoApplicantName: "setCoApplicantName",
+      setCompanyName: "setCompanyName",
+      setDate: "setDate",
+      setHouseToPaintAddress: "setHouseToPaintAddress",
+      setHouseToPaintCity: "setHouseToPaintCity",
+      setHouseToPaintState: "setHouseToPaintState",
+      setHouseToPaintZipcode: "setHouseToPaintZipcode",
+      setIsHouseToPaintSameAsPersonal: "setIsHouseToPaintSameAsPersonal",
+      setName: "setName",
+      setPersonalAddress: "setPersonalAddress",
+      setPersonalCity: "setPersonalCity",
+      setPersonalState: "setPersonalState",
+      setPersonalZipcode: "setPersonalZipcode",
+    }),
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,114 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    customerEstimate: {
+      exterior: {
+
+      },
+      interior: {
+
+      },
+      personalInformation: {
+        coApplicantName: "",
+        companyName: "",
+        date: new Date(),
+        houseToPaintAddress: "",
+        houseToPaintCity: "",
+        houseToPaintState: "",
+        houseToPaintZipcode: "",
+        isHouseToPaintSameAsPersonal: false,
+        name: "",
+        personalAddress: "",
+        personalCity: "",
+        personalState: "",
+        personalZipcode: ""
+      },
+      totalPayment: {
+
+      }
+    }
+  },
+  getters: {
+    getCoApplicantName(state) {
+      return state.coApplicantName;
+    },
+    getCompanyName(state) {
+      return state.companyName;
+    },
+    getDate(state) {
+      return state.date;
+    },
+    getHouseToPaintAddress(state) {
+      return state.houseToPaintAddress;
+    },
+    getHouseToPaintCity(state) {
+      return state.houseToPaintCity;
+    },
+    getHouseToPaintState(state) {
+      return state.houseToPaintState;
+    },
+    getHouseToPaintZipcode(state) {
+      return state.houseToPaintZipcode;
+    },
+    getIsHouseToPaintSameAsPersonal(state) {
+      return state.isHouseToPaintSameAsPersonal;
+    },
+    getName(state) {
+      return state.name;
+    },
+    getPersonalAddress(state) {
+      return state.personalAddress;
+    },
+    getPersonalCity(state) {
+      return state.personalCity;
+    },
+    getPersonalState(state) {
+      return state.personalState;
+    },
+    getPersonalZipcode(state) {
+      return state.personalZipcode;
+    }
   },
   mutations: {
+    setCoApplicantName(state, val) {
+      state.coApplicantName = val;
+    },
+    setCompanyName(state, val) {
+      state.companyName = val;
+    },
+    setDate(state, val) {
+      state.date = val;
+    },
+    setHouseToPaintAddress(state, val) {
+      state.houseToPaintAddress = val;
+    },
+    setHouseToPaintCity(state, val) {
+      state.houseToPaintCity = val;
+    },
+    setHouseToPaintState(state, val) {
+      state.houseToPaintState = val;
+    },
+    setHouseToPaintZipcode(state, val) {
+      state.houseToPaintZipcode = val;
+    },
+    setIsHouseToPaintSameAsPersonal(state, val) {
+      state.isHouseToPaintSameAsPersonal = val;
+    },
+    setName(state, val) {
+      state.name = val;
+    },
+    setPersonalAddress(state, val) {
+      state.personalAddress = val;
+    },
+    setPersonalCity(state, val) {
+      state.personalCity = val;
+    },
+    setPersonalState(state, val) {
+      state.personalState = val;
+    },
+    setPersonalZipcode(state, val) {
+      state.personalZipcode = val;
+    }
   },
   actions: {
   },
